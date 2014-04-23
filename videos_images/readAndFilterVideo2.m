256022737227cf62eeccc1ce1773b4bf7900fb1c
Stephen Lazzaro <slazzaro@knuckles.cs.wisc.edu>
2014-04-23 13:01:06 -0500

    update functions and create new ones

--- videos_images/readAndFilterVideo2.m
+++ videos_images/readAndFilterVideo2.m
@@ -1,4 +1,4 @@
-function [ gradientImages ] = readAndFilterVideo(name, centroids)
+function [ gradientImages ] = readAndFilterVideo2(name, centroids)
 %   Function: readAndFilterVideo
 %   Author : Stephen Lazzaro
 %   Description: Takes the input centroids and places them in the
@@ -26,7 +26,7 @@ vidObj = VideoReader(name);
 % based off http://www.mathworks.com/help/imaq/examples/live-motion-detection-using-optical-flow.html
 optical = vision.OpticalFlow( ...
 'OutputValue', 'Horizontal and vertical components in complex form', ...
-    'ReferenceFrameSource', 'Input Port');
+    'ReferenceFrameSource', 'Input Port', 'Smoothness', 10);
 
 hVideoIn = vision.VideoPlayer;
 hVideoIn.Name  = 'Tracking Video';
@@ -55,6 +55,47 @@ nframes = vidObj.NumberOfFrames;
 batchDivider = 50;
 numCentroidsShowing = 0;
 numCentroidsUnused = size(centroids,1);
+
+%TESTT
+
+sz = get(0,'ScreenSize');
+pos = [20 sz(4)-300 200 200];
+hVideo1 = vision.VideoPlayer('Name','Original Video','Position',pos);
+pos(1) = pos(1)+220; % move the next viewer to the right
+hVideo2 = vision.VideoPlayer('Name','Motion Vector','Position',pos);
+pos(1) = pos(1)+220;
+hVideo3 = vision.VideoPlayer('Name','Thresholded Video','Position',pos);
+pos(1) = pos(1)+220;
+hVideo4 = vision.VideoPlayer('Name','Results','Position',pos);
+
+hMean1 = vision.Mean;
+hMean2 = vision.Mean('RunningMean', true);
+hMedianFilt = vision.MedianFilter;
+hclose = vision.MorphologicalClose('Neighborhood', strel('line',5,45));
+hblob = vision.BlobAnalysis(...
+    'CentroidOutputPort', false, 'AreaOutputPort', true, ...
+    'BoundingBoxOutputPort', true, 'OutputDataType', 'double', ...
+    'MinimumBlobArea', 250, 'MaximumBlobArea', 3600, 'MaximumCount', 80);
+herode = vision.MorphologicalErode('Neighborhood', strel('square',2));
+hshapeins1 = vision.ShapeInserter('BorderColor', 'Custom', ...
+                                  'CustomBorderColor', [0 1 0]);
+hshapeins2 = vision.ShapeInserter( 'Shape','Lines', ...
+                                   'BorderColor', 'Custom', ...
+                                   'CustomBorderColor', [255 255 0]);
+htextins = vision.TextInserter('Text', '%4d', 'Location',  [1 1], ...
+                               'Color', [1 1 1], 'FontSize', 12);
+
+% Initialize variables used in plotting motion vectors.
+lineRow   =  22;
+firstTime = true;
+motionVecGain  = 20;
+borderOffset   = 5;
+decimFactorRow = 5;
+decimFactorCol = 5;
+                               
+%END TEST
+
+
 for i = 1:batchDivider
     start = ceil( (i - 1) * nframes/batchDivider);
     display(strcat('INFO : Reading Video Frame:', num2str(start)));
@@ -91,15 +132,71 @@ for i = 1:batchDivider
         %optFlow = step(optical, double(rangefilt(gray2)), double(rangefilt(gray1)));
         optFlow = step(optical, double(gray2), double(gray1));
         
-%         % Downsample optical flow field.
-        optFlow_DS = optFlow(r, c);
-        H = imag(optFlow_DS)*50;
-        V = real(optFlow_DS)*50;
+        %TESTTTTTTTTT
         
-        % Draw lines on top of image
-        %lines = [Y(:)'; X(:)'; Y(:)'+V(:)'; X(:)'+H(:)'];
-        lines = [Y(:)'; X(:)'; Y(:)'; X(:)'];
-        rgb_Out = step(shapes, double(rangefilt(gray2)),  lines');
+        y1 = optFlow .* conj(optFlow);
+        % Compute the velocity threshold from the matrix of complex velocities.
+        vel_th = 0.5 * step(hMean2, step(hMean1, y1));
+        
+        % Threshold the image and then filter it to remove speckle noise.
+        segmentedObjects = step(hMedianFilt, y1 >= vel_th);
+        
+        % Thin-out the parts of the road and fill holes in the blobs.
+        segmentedObjects = step(hclose, step(herode, segmentedObjects));
+        
+        % Estimate the area and bounding box of the blobs.
+        [area, bbox] = step(hblob, segmentedObjects);
+        % Select boxes inside ROI (below white line).
+        Idx = bbox(:,1) > lineRow;
+        
+        % Based on blob sizes, filter out objects which can not be cars.
+        % When the ratio between the area of the blob and the area of the
+        % bounding box is above 0.4 (40%), classify it as a car.
+        ratio = zeros(length(Idx), 1);
+        ratio(Idx) = single(area(Idx,1))./single(bbox(Idx,3).*bbox(Idx,4));
+        ratiob = ratio > 0.3;
+        count = int32(sum(ratiob));    % Number of cars
+        bbox(~ratiob, :) = int32(-1);
+        
+        % Draw bounding boxes around the tracked cars.
+        y2 = step(hshapeins1, rgbData, bbox);
+        
+        % Display the number of cars tracked and a white line showing the ROI.
+        y2(22:23,:,:)   = 1;   % The white line.
+        y2(1:15,1:30,:) = 0;   % Background for displaying count
+        result = step(htextins, y2, count);
+        
+        % Generate coordinates for plotting motion vectors.
+        if firstTime
+            [R C] = size(optFlow);            % Height and width in pixels
+            RV = borderOffset:decimFactorRow:(R-borderOffset);
+            CV = borderOffset:decimFactorCol:(C-borderOffset);
+            [Y X] = meshgrid(CV,RV);
+            firstTime = false;
+        end
+        
+        % Calculate and draw the motion vectors.
+        tmp = optFlow(RV,CV) .* motionVecGain;
+        lines = [Y(:), X(:), Y(:) + real(tmp(:)), X(:) + imag(tmp(:))];
+        motionVectors = step(hshapeins2, single(rgbData), lines);
+        
+        % Display the results
+        %step(hVideo1, rgbData);            % Original video
+        step(hVideo2, motionVectors);    % Video with motion vectors
+        %step(hVideo3, segmentedObjects); % Thresholded video
+        step(hVideo4, result);           % Video with bounding boxes
+        
+        %END TESTTT
+        
+%         % Downsample optical flow field.
+%         optFlow_DS = optFlow(r, c);
+%         H = imag(optFlow_DS)*50;
+%         V = real(optFlow_DS)*50;
+%         
+%         % Draw lines on top of image
+%         %lines = [Y(:)'; X(:)'; Y(:)'+V(:)'; X(:)'+H(:)'];
+%         lines = [Y(:)'; X(:)'; Y(:)'; X(:)'];
+%         rgb_Out = step(shapes, double(rangefilt(gray2)),  lines');
         
         %Allow this for centroid to be displayed
         %TODO: must handle if part of centroid off screen
@@ -121,15 +218,15 @@ for i = 1:batchDivider
         
         % Send image data to video player
         % Display original video with centroids added
-        step(hVideoIn, rgbData);
+        %step(hVideoIn, rgbData);
         
         % Display video along with motion vectors.
         %step(hVideoOut, rgb_Out);
         
         % Increment frame count
-        rgbData = rgbData2; %do this so you don't have to read each file on two iterations
+        %rgbData = rgbData2; %do this so you don't have to read each file on two iterations
         frame = frame + 1;
-        
+        rgbData = read(vidObj, frame);
         % Finally, shift each of centroidsShowing based on optical
         %flow field.  Note: if the centroid has any coordinate out of
         %bounds then remove it from the centroidsShowingArray and decrement
